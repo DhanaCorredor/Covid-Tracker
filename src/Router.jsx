6a962ea68a8e68f63c90e839007ca6bdfe474197
unknown <luisafer9320@gmail.com>
2026-05-08 11:57:14 +0200

    cambio Router.jsx

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,12 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import { ApiTest } from "./components/common/ApiTest";
-<<<<<<< HEAD
-import DashboardLayout from "./pages/DashboardLayout";
 import { Tracker3 } from "./pages/Tracker3";
-=======
 import { DashboardLayout } from "./pages/DashboardLayout";
 import { Tracker1 } from "./pages/Tracker1";
->>>>>>> 40784e514065816877ea5fc4332eafc7acc27f95
 
 const Placeholder = ({ name }) => <h1 className="p-4">{name}</h1>;
 
