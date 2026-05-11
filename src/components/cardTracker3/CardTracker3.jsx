5818258f1805b2cdb31cc23ff9acbc05123a844b
unknown <luisafer9320@gmail.com>
2026-05-11 09:14:33 +0200

    container cardTracker3

--- src/components/cardTracker3/CardTracker3.jsx
+++ src/components/cardTracker3/CardTracker3.jsx
@@ -1,9 +1,69 @@
-import "./cardtracker3.css"
+import React, { useState, useEffect } from 'react'
+import './cardTracker3.css'
+
+const CardCountry = ({ flagUrl, countryName, totalCases }) => {
+    return (
+        <div className="card-country-container">
+            <img src={flagUrl} alt={countryName} className="country-flag" />
+            <h3 className="country-name">{countryName}</h3>
+            <p className="country-total-cases">{totalCases}</p>
+        </div>
+    )
+}
+
+export const TopTenCountries = () => {
+
+    let api_url = "https://disease.sh/v3/covid-19/countries"
+
+    // Hook UseState - Mantiene el estado del componente
+    const [countries, setCountries] = useState()
+    const [loading, setLoading] = useState(true)
+    console.log(countries)
+
+    // Hook UseEffect - Monta el componente en base a un estado
+    useEffect(() => {
+        const api = async (url) => {
+            const result = await fetch(url);
+            let covid_data = await result.json();
+            setCountries(covid_data);
+            setLoading(false)
+        };
+        api(api_url)
+    }, [api_url])
+
+    if (loading) {
+        return <h2>Loading...</h2>
+    }
+
+    return (
+        <div className="top-10-container">
+            <h2 className="top-10-title">Total Cases</h2>
+            <h2>Today Cases</h2>
+            <h2>Today Deaths</h2>
+            <h2>Today deaths</h2>
+            <h2>Top active </h2>
+            <h2>Top Recovered</h2>
+            <div className="top-10-list">
+                {countries.map((country) => (
+                    <CardCountry key={country.country}
+                        flagUrl={country.countryInfo.flag}
+                        countryName={country.country}
+                        totalCases={country.cases} />
+                ))
+                }
+            </div>
+        </div>
+    )
+}
 
 export const CardTracker3 = () => {
-    return (<div className="countainerDateCovid">
-            <h2 className="text-heading-lg font-bold text-gray-800">COVID-19 Tracker</h2>
-            <p className="text-body-md text-gray-600">Top 10 Country wise Covid-19 Update - Tiles</p>
-    </div>
-        )
-    }
+    return (
+        <div className="countainerDateCovid">
+            <h3 className="text-heading-lg font-bold text-gray-800">Top 10 Country wise Covid-19 Updates - Tiles</h3>
+            <TopTenCountries />
+        </div>
+    )
+}
+
+
+
--- src/components/layout/dataSidebar/CountryRow.jsx
+++ src/components/layout/dataSidebar/CountryRow.jsx
@@ -4,7 +4,7 @@ import { formatNumber } from  '../../../utils/format'
 
 export const CountryRow = ({ flag, name, value }) => {
   return (
-    <div className="flex items-center justify-between gap-4  w-full mb-2 p-4  transform transition-all duration-500 ease-in-out hover:-translate-y-1 bg-white rounded-xs shadow-sm border border-gray-100 mb-2">
+    <div className="flex items-center justify-between gap-4  w-full mb-3 p-4  transform transition-all duration-500 ease-in-out hover:-translate-y-1 bg-white rounded-xs shadow-sm border border-gray-100 mb-2">
       <div className="flex items-center gap-4">
         <img 
           src={flag} 
